101e70fc3a839ec3a54b98314962b2edd88db362
Donttu <donttuboi@gmail.com>
2024-12-09 19:08:02 +0200

    Day 09 solution

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -18,22 +18,13 @@ namespace Day09
 
             Console.WriteLine("Enter the input (separated by newline):");
 
-            string input = string.Empty;
-            string line;
-            while ((line = Console.ReadLine() ?? string.Empty) != string.Empty)
-            {
-                input += line + Environment.NewLine;
-            }
-
-            string[] lines = input.Trim().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-
             switch (choice)
             {
                 case "1":
-                    Part1.Execute(lines);
+                    Part1.Execute();
                     break;
                 case "2":
-                    Part2.Execute(lines);
+                    Part2.Execute();
                     break;
                 default:
                     Console.WriteLine("Invalid choice. Please choose a valid part.");
@@ -44,11 +35,10 @@ namespace Day09
 
     public class Part1 // 6337921897505
     {
-        public static void Execute(string[] asd)
+        public static void Execute()
         {
             string inputFile = Path.GetFullPath(@"..\..\..\input.txt");
             string input = File.ReadAllText(inputFile);
-            //input = string.Join("", asd);
 
             List<int> numbers = new List<int>();
             foreach (char x in input)
@@ -137,11 +127,10 @@ namespace Day09
 
     public class Part2 // 6362722604045
     {
-        public static void Execute(string[] lines)
+        public static void Execute()
         {
             string inputFile = Path.GetFullPath(@"..\..\..\input.txt");
             string input = File.ReadAllText(inputFile);
-            //input = string.Join("", asd);
 
             List<int> numbers = new List<int>();
             foreach (char x in input)
@@ -160,7 +149,6 @@ namespace Day09
 
             for (int i = 0, j = 0; i < numbers.Count; i++, j++)
             {
-                if (j == 10) { j = 0; }
                 int digit = Int32.Parse(numbers[i].ToString());
                 for (int k = 0; k < digit; k++)
                 {
@@ -169,56 +157,86 @@ namespace Day09
                 i++;
                 if (i >= numbers.Count) { break; }
                 digit = Int32.Parse(numbers[i].ToString());
-                if (j != 9)
+                for (int k = 0; k < digit; k++)
                 {
-                    for (int k = 0; k < digit; k++)
-                    {
-                        diskLayout.Add('.'.ToString());
-                    }
+                    diskLayout.Add('.'.ToString());
                 }
             }
 
-            Console.WriteLine("The disk layout is:");
-            Console.WriteLine(string.Join("", diskLayout));
-
-            /*
-            TODO: Implement a new type of moving algorithm that moves whole files instead of single blocks.
-            A file is basicly all the blocks that have the same value.
-            Starting from the file with the highest ID number, we try to see if we can fit that file to a gap of dots to the left of it.
-            If there is no gap that is big enough for the single file, we move on to the next file that has an ID that is one lower than the previous one, and leave the original file at its place.
-            If we move a file from right to left, the space that the file used to take should be preserved.
-            Every file should be tried to moved exatcly once in order of decreasing file ID number.
-            */
-
-            int highestFileId = 0;
+            int highestFieldId = 0;
             foreach (string fileId in diskLayout)
             {
-                if (fileId == ".") { continue; }
-                if (int.Parse(fileId) > highestFileId)
+                if (fileId == ".")
                 {
-                    highestFileId = int.Parse(fileId);
+                    continue;
+                }
+                if (int.Parse(fileId) > highestFieldId)
+                {
+                    highestFieldId = int.Parse(fileId);
                 }
             }
-            int amountOfFiles = diskLayout.Count(x => x == highestFileId.ToString());
 
-            Console.WriteLine($"Highest File ID: {highestFileId}");
-            Console.WriteLine($"Amount of Files with Highest ID: {amountOfFiles}");
-            
-            Console.WriteLine("The disk layout after moving file blocks is:");
-            Console.WriteLine(string.Join("", diskLayout));
+            while (highestFieldId >= 0)
+            {
+                int highFileId = highestFieldId;
+                int amountOfFiles = diskLayout.Count(x => x == highFileId.ToString());
+                int requiredGapSize = amountOfFiles;
+                int highFileIdIndex = -1;
 
-            Console.WriteLine(diskLayout.Count);
+                for (int i = 0; i < diskLayout.Count; i++)
+                {
+                    if (diskLayout[i] == highFileId.ToString())
+                    {
+                        highFileIdIndex = i;
+                        break;
+                    }
+                }
+
+
+                int gapStartIndex = -1;
+                int actualGapSize = 0;
+                for (int i = 0; i < highFileIdIndex; i++)
+                {
+                    if (diskLayout[i] == ".")
+                    {
+                        actualGapSize++;
+                        if (actualGapSize == requiredGapSize)
+                        {
+                            gapStartIndex = i - requiredGapSize + 1;
+                            break;
+                        }
+                    }
+                    else if (diskLayout[i] != ".")
+                    {
+                        actualGapSize = 0;
+                    }
+                }
+
+                if (gapStartIndex != -1 && highFileIdIndex != -1)
+                {
+                    for (int i = 0; i < amountOfFiles; i++)
+                    {
+                        string file = diskLayout[highFileIdIndex + i];
+                        string dot = diskLayout[gapStartIndex + i];
+                        diskLayout[highFileIdIndex + i] = dot;
+                        diskLayout[gapStartIndex + i] = file;
+                    }
+                }
+
+                highestFieldId--;
+            }
+
+
 
             long checksum = 0;
-            /*
+
             for (int i = 0; i < diskLayout.Count; i++)
             {
                 if (char.IsDigit(diskLayout[i][0]))
                 {
-                    checksum += i * (diskLayout[i][0] - '0');
+                    checksum += i * Int32.Parse(diskLayout[i]);
                 }
             }
-            */
 
             Console.WriteLine("The checksum is: " + checksum);
             Console.WriteLine("Part 2 executed.");
